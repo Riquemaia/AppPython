3cbde679d0f23d19a5501a9165b2170c60360775
Riquemaia <45611685+Riquemaia@users.noreply.github.com>
2023-05-24 16:02:56 -0300

    Update json_api.py

--- json_api.py
+++ json_api.py
@@ -77,3 +77,57 @@ schema_users=StructType(
                     ])))
                 ]
             )
+
+schema_activity= StructType(
+                [
+                    StructField('Id', StringType(), True),
+                    StructField('RecordType', StringType(), True),
+                    StructField('CreationTime', StringType(), True),
+                    StructField('Operation', StringType(), True),
+                    StructField('OrganizationId', StringType(), True),
+                    StructField('UserType', StringType(), True),
+                    StructField('UserKey', StringType(), True),
+                    StructField('Workload', StringType(), True),
+                    StructField('UserId', StringType(), True),
+                    StructField('ClientIP', StringType(), True),
+                    StructField('Activity', StringType(), True),
+                    StructField('ItemName', StringType(), True),
+                    StructField('WorkSpaceName', StringType(), True),
+                    StructField('CapacityId', StringType(), True),
+                    StructField('CapacityName', StringType(), True),
+                    StructField('WorkspaceId', StringType(), True),
+                    StructField('ObjectId', StringType(), True),
+                    StructField('DataflowId', StringType(), True),
+                    StructField('DataflowName', StringType(), True),
+                    StructField('IsSuccess', StringType(), True),
+                    StructField('DataflowRefreshScheduleType', StringType(), True),
+                    StructField('DataflowType', StringType(), True),
+                    StructField('RequestId', StringType(), True),
+                    StructField('ActivityId', StringType(), True),
+                    StructField('UserAgent', StringType(), True),
+                    StructField('DataflowAccessTokenRequestParameters', StructType([
+                        StructField("tokenLifetimeInMinutes", StringType(), True),
+                        StructField('permissions', StringType(), True),
+                        StructField('entityName', StringType(), True),
+                        StructField('partitionUri', StringType(), True)
+                        ])),
+                    StructField('DatasetName', StringType(), True),
+                    StructField('ReportName', StringType(), True),
+                    StructField('AppName', StringType(), True),
+                    StructField('DatasetId', StringType(), True),
+                    StructField('ReportId', StringType(), True),
+                    StructField('ArtifactId', StringType(), True),
+                    StructField('ArtifactName', StringType(), True),
+                    StructField('ReportType', StringType(), True),
+                    StructField('AppReportId', StringType(), True),
+                    StructField('DistributionMethod', StringType(), True),
+                    StructField('ConsumptionMethod', StringType(), True),
+                    StructField('AppId', StringType(), True),
+                    StructField('ArtifactKind', StringType(), True),
+                    StructField('EndPoint', StringType(), True),
+                    StructField('DataConnectivityMode', StringType(), True),
+                    StructField('RefreshType', StringType(), True),
+                    StructField('LastRefreshTime', StringType(), True),
+                    StructField('ModelsSnapshots', StringType(), True),
+                ]
+            )
